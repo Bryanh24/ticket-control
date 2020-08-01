20a71dc26fb0f7bebb546e5b84a66da000a279d0
Bryan Hernandez <bryanh24@hotmail.com>
2020-08-01 13:34:35 -0600

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://bryanh24:Comun123*@cluster0.s504h.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URL
 }
 process.env.URLDB = urlDB;
