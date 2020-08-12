c8c1c60c8c635bc5c369f8f9a32e86f5b023a66d
Bryan Hernandez <bryanh24@hotmail.com>
2020-08-11 21:42:22 -0600

    Subida de archivos

--- server/middlwares/auth.js
+++ server/middlwares/auth.js
@@ -1,6 +1,7 @@
 const { JsonWebTokenError } = require("jsonwebtoken");
 
 const jwt = require("jsonwebtoken");
+const { json } = require("body-parser");
 
 //========
 //VERIFICAR TOKEN
@@ -28,20 +29,40 @@ let verificaToken = (req, res, next) => {
 
 let verificaAdminRole = (req, res, next) => {
 
-    let usuario = req.usuario
+        let usuario = req.usuario
 
-    if (usuario.role === 'ADMIN_ROLE') {
-        next();
-    } else {
-        return res.json({
-            ok: false,
-            err: {
-                message: 'No tiene los permisos para realizar esta accion.'
-            }
-        })
+        if (usuario.role === 'ADMIN_ROLE') {
+            next();
+        } else {
+            return res.json({
+                ok: false,
+                err: {
+                    message: 'No tiene los permisos para realizar esta accion.'
+                }
+            })
+        }
+
+
     }
+    //=====================
+    //VERIFICA PARA IMAGEN
+    //=====================
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: 'token no valido'
+            });
+        }
 
-
+        req.usuario = decoded.usuario;
+        next();
+    });
 }
-
-module.exports = { verificaToken, verificaAdminRole };
+module.exports = {
+    verificaToken,
+    verificaAdminRole,
+    verificaTokenImg,
+};
